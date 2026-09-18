9911e7ec316ae6c48284db2bb2e5dd81f87dae60
bdoobd <G3xAg0N@mail.ru>
2026-09-18 15:44:35 +0300

    NEW: Links as tile

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -22,11 +22,12 @@ const linkCard = function (data) {
   return `
     <div class="card__link">
       <div class="card__header">
-        <h3><a href="#">Title</a></h3><h4>Category</h4>
+        <h3><a href="${data.url}">${data.title}</a></h3>
       </div>
       <div class="card__body">
-        Keikkalista is a tool used for managing and scheduling work shifts. It allows employees to view their assigned shifts, request changes, and communicate with their team. The platform helps streamline workforce management and improve communication within the organization.
+        ${data.description} 
       </div>
+      <div class="card__footer"><h4>${data.category}</h4></div>
     </div>
   `;
 };
@@ -42,13 +43,21 @@ export async function initHome() {
 
   const links = await linkData();
 
-  console.log(links.links);
+  let data = links.links;
+  // TODO: Сделать функцию получения ссылок. Записать результат в переменную. Сделать функцию отделения из файла значений свойства category в отдельный массив. Этот массив использовать для формирования разделов для ссылок. В дальнейшем отображать ссылки в тех раздела, которые указанны в них
+  data = data.sort((a, b) => {
+    if (a.category < b.category) {
+      return -1;
+    }
+  });
+
+  console.log(data);
 
   let markup = "";
 
-  for (let i = 1; i < 20; i++) {
-    markup = markup + linkCard(links);
-  }
+  links.links.forEach((element) => {
+    markup = markup + linkCard(element);
+  });
 
   grid.innerHTML = markup;
 }
